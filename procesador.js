5aef4e23d4b8a37641b0c6e608a1dd634c294ac1
alboncho <xetres3@gmail.com>
2026-04-21 21:52:10 -0400

    con console.log

--- procesador.js
+++ procesador.js
@@ -3,5 +3,5 @@ let box_procesador =  document.getQuerySelector('.box-procesador');
 if (box_procesador) {
    box_procesador.innerHTML = 'hola';
 } else {
-   alert('No existe el contenedor');
+   console.log("No existe el contenedor');
 }
