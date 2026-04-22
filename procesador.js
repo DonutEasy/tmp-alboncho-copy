4e32c24f43bf212806fbac20c311d2eb39ad2975
alboncho <xetres3@gmail.com>
2026-04-21 21:47:23 -0400

    agregando la condicional

--- procesador.js
+++ procesador.js
@@ -1,1 +1,7 @@
 let box_procesador =  document.getQuerySelector('.box-procesador');
+
+if (box_procesador) {
+   box_procesador.innerHTML = 'hola';
+} else {
+   alert('No existe el contenedor');
+}
