c48a125920162619bf90deb078f048ccdb24ea43
alboncho <xetres3@gmail.com>
2026-05-24 09:20:22 -0400

    echo  > procesador.js

--- procesador.js
+++ procesador.js
@@ -1,8 +1,1 @@
-let box_procesador =  document.getQuerySelector('.box-procesador');
 
-if (box_procesador) {
-   console.log("existe");
-} else {
-   console.log("No existe el contenedor');
-   let x = documente.createElement('div');
-}
